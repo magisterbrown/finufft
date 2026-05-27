99e9237bb1c94926334fb597d39ee1bf95df7edd
Artur <magisterbrownie@gmail.com>
2026-05-27 18:48:19 +0200

    change

--- include/finufft/setpts.hpp
+++ include/finufft/setpts.hpp
@@ -107,9 +107,6 @@ int FINUFFT_PLAN_T<TF>::setpts(BIGINT nj, const TF *xj, const TF *yj, const TF *
     if (!upsamp_locked) {
       double density   = double(nj) / double(N());
       double upsampfac = bestUpsamplingFactorComplexity<TF>(nj, dim, m.tol, type, mstu);
-
-      // double upsampfac = bestUpsamplingFactor<TF>(opts.nthreads, density, dim, type,
-      // m.tol);
       //  Re-plan if this is the first call (upsampfac==0) or if upsampfac changed
       if (upsampfac != opts.upsampfac) {
         opts.upsampfac = upsampfac;
